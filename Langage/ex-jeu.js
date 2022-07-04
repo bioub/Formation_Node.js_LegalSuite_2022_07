6022f4e4c8faeb2ed5c4d24843369a4f3824fda3
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-07-04 16:13:36 +0200

    Correction jeu

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,3 +1,24 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+
 // importe le module readline
 const readline = require('readline');
 
@@ -7,14 +28,34 @@ const rl = readline.createInterface({
 });
 
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '));
+  }
   rl.question('Quel est le nombre ? ', (answer) => {
     console.log('Vous avez saisi : ' + answer);
 
-    // pour rejouer
-    jouer();
+    const entierSaisi = parseInt(answer, 10);
 
-    // pour terminer la partie
-    // rl.close();
+    if (isNaN(entierSaisi)) {
+      console.log('Il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierAlea < entierSaisi) {
+      console.log('Trop grand');
+      jouer();
+    } else if (entierAlea > entierSaisi) {
+      console.log('Trop petit');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
+
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
