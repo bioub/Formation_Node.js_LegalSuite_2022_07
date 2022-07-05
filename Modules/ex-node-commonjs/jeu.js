3eb4b70613a223674036ef8bac8ab9518a0d0994
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-07-05 16:29:15 +0200

    NPM

--- Modules/ex-node-commonjs/jeu.js
+++ Modules/ex-node-commonjs/jeu.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const chalk = require('chalk');
 const { getRandomInt } = require('./random');
 
 class Jeu {
@@ -18,26 +19,26 @@ class Jeu {
     if (this.essais.length) {
       console.log('Vous avez déjà joué : ' + this.essais.join(' - '));
     }
-    this.rl.question('Quel est le nombre ? ', (answer) => {
-      console.log('Vous avez saisi : ' + answer);
+    this.rl.question(chalk.blue('Quel est le nombre ? '), (answer) => {
+      console.log(chalk.underline('Vous avez saisi : ' + answer));
 
       const entierSaisi = parseInt(answer, 10);
 
       if (isNaN(entierSaisi)) {
-        console.log('Il faut saisir un nombre');
+        console.log(chalk.red('Il faut saisir un nombre'));
         return this.jouer();
       }
 
       this.essais.push(entierSaisi);
 
       if (this.entierAlea < entierSaisi) {
-        console.log('Trop grand');
+        console.log(chalk.yellow('Trop grand'));
         this.jouer();
       } else if (this.entierAlea > entierSaisi) {
-        console.log('Trop petit');
+        console.log(chalk.yellow('Trop petit'));
         this.jouer();
       } else {
-        console.log('Gagné');
+        console.log(chalk.green('Gagné'));
         this.rl.close();
       }
     });
