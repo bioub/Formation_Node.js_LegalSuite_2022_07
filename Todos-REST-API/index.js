5f551ed25b5a305063fe8f22869621856eb6893b
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-07-07 14:38:49 +0200

    Mongoose

--- Todos-REST-API/index.js
+++ Todos-REST-API/index.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const http = require("http");
+const mongoose = require("mongoose");
 
-const config = require('./config');
-const app  = require('./app');
+const config = require("./config");
+const app = require("./app");
 
 const server = http.createServer(app);
 
-server.on('error', (err) => {
+server.on("error", (err) => {
   console.log(err.message);
 });
 
-server.listen(config.port, () => {
-  console.log('Server started on port ' + config.port);
+mongoose.connect("mongodb://localhost:27017/test").then(() => {
+  server.listen(config.port, () => {
+    console.log("Server started on port " + config.port);
+  });
 });
--- Todos-REST-API/models/todo.js
+++ Todos-REST-API/models/todo.js
@@ -1,76 +1,108 @@
-const todos = [
-  {
-    id: 1,
-    title: 'Acheter du pain',
-    completed: false,
+// const todos = [
+//   {
+//     id: 1,
+//     title: 'Acheter du pain',
+//     completed: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Introduire Express',
+//     completed: true,
+//   }
+// ];
+
+// function generateId() {
+//   const maxId = todos.reduce((acc, c) => c.id > acc ? c.id : acc, 0);
+//   return maxId + 1;
+// }
+
+// function find() {
+//   return Promise.resolve(todos);
+// }
+
+// function findById(id) {
+//   id = Number(id);
+//   const todo = todos.find((c) => c.id === id);
+
+//   if (!todo) {
+//     return Promise.resolve(null);
+//   }
+
+//   return Promise.resolve(todo);
+// }
+
+// function create(todo) {
+//   todo.id = generateId();
+
+//   todos.push(todo);
+
+//   return Promise.resolve(todo);
+// }
+
+// function findByIdAndDelete(id) {
+//   id = Number(id);
+//   const todo = todos.find((c) => c.id === id);
+
+//   if (!todo) {
+//     return Promise.resolve(null);
+//   }
+
+//   const index = todos.indexOf(todo);
+//   todos.splice(index, 1);
+
+//   return Promise.resolve(todo);
+// }
+
+
+// function findByIdAndUpdate(id, newTodo) {
+//   id = Number(id);
+//   const todo = todos.find((c) => c.id === id);
+//   newTodo.id = id;
+
+//   if (!todo) {
+//     return Promise.resolve(null);
+//   }
+
+//   const index = todos.indexOf(todo);
+//   todos[index] = newTodo;
+
+//   return Promise.resolve(todo);
+// }
+
+// exports.find = find;
+// exports.findById = findById;
+// exports.create = create;
+// exports.findByIdAndDelete = findByIdAndDelete;
+// exports.findByIdAndUpdate = findByIdAndUpdate;
+
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    id: 2,
-    title: 'Introduire Express',
-    completed: true,
+  completed: {
+    type: Boolean,
+    default: false,
   }
-];
+});
 
-function generateId() {
-  const maxId = todos.reduce((acc, c) => c.id > acc ? c.id : acc, 0);
-  return maxId + 1;
-}
+const Todo = model('Todo', schema);
 
-function find() {
-  return Promise.resolve(todos);
-}
+module.exports = Todo;
 
-function findById(id) {
-  id = Number(id);
-  const todo = todos.find((c) => c.id === id);
+// Méthodes statiques
+// https://mongoosejs.com/docs/models.html
+// Todo.create
+// Todo.find
+// Todo.findOne
+// Todo.findById
+// Todo.findByIdAndUpdate
+// Todo.deleteMany
 
-  if (!todo) {
-    return Promise.resolve(null);
-  }
-
-  return Promise.resolve(todo);
-}
-
-function create(todo) {
-  todo.id = generateId();
-
-  todos.push(todo);
-
-  return Promise.resolve(todo);
-}
-
-function findByIdAndDelete(id) {
-  id = Number(id);
-  const todo = todos.find((c) => c.id === id);
-
-  if (!todo) {
-    return Promise.resolve(null);
-  }
-
-  const index = todos.indexOf(todo);
-  todos.splice(index, 1);
-
-  return Promise.resolve(todo);
-}
-
-
-function findByIdAndUpdate(id, newTodo) {
-  id = Number(id);
-  const todo = todos.find((c) => c.id === id);
-  newTodo.id = id;
-
-  if (!todo) {
-    return Promise.resolve(null);
-  }
-
-  const index = todos.indexOf(todo);
-  todos[index] = newTodo;
-
-  return Promise.resolve(todo);
-}
-
-exports.find = find;
-exports.findById = findById;
-exports.create = create;
-exports.findByIdAndDelete = findByIdAndDelete;
-exports.findByIdAndUpdate = findByIdAndUpdate;
+// Méthode du prototype
+// https://mongoosejs.com/docs/documents.html
+// const todo = new Todo({title: 'ABC'});
+// todo.save()
+// todo.delete()
